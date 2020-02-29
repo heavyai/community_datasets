d8dd470ca12d1b27456a96fb759b55a86fe6a148
Randy Zwitch <randy.zwitch@omnisci.com>
2020-02-28 20:05:24 -0500

    Remove space in statement

--- caltrans_2015_2019/omnisci_ddl/caltrans_historic_2015_2019.sql
+++ caltrans_2015_2019/omnisci_ddl/caltrans_historic_2015_2019.sql
@@ -22,8 +22,8 @@ day_of_year SMALLINT
 )
 
 --copy table statements for omnisql
-copy caltrans_historic_2015_2019 from ' s3://omnisci-community/datasets/caltrans_2015_2019/caltrans_2015.csv.gz' with (header='true');
-copy caltrans_historic_2015_2019 from ' s3://omnisci-community/datasets/caltrans_2015_2019/caltrans_2016.csv.gz' with (header='true');
-copy caltrans_historic_2015_2019 from ' s3://omnisci-community/datasets/caltrans_2015_2019/caltrans_2017.csv.gz' with (header='true');
-copy caltrans_historic_2015_2019 from ' s3://omnisci-community/datasets/caltrans_2015_2019/caltrans_2018.csv.gz' with (header='true');
-copy caltrans_historic_2015_2019 from ' s3://omnisci-community/datasets/caltrans_2015_2019/caltrans_2019_partial.csv.gz' with (header='true');
+copy caltrans_historic_2015_2019 from 's3://omnisci-community/datasets/caltrans_2015_2019/caltrans_2015.csv.gz' with (header='true');
+copy caltrans_historic_2015_2019 from 's3://omnisci-community/datasets/caltrans_2015_2019/caltrans_2016.csv.gz' with (header='true');
+copy caltrans_historic_2015_2019 from 's3://omnisci-community/datasets/caltrans_2015_2019/caltrans_2017.csv.gz' with (header='true');
+copy caltrans_historic_2015_2019 from 's3://omnisci-community/datasets/caltrans_2015_2019/caltrans_2018.csv.gz' with (header='true');
+copy caltrans_historic_2015_2019 from 's3://omnisci-community/datasets/caltrans_2015_2019/caltrans_2019_partial.csv.gz' with (header='true');
